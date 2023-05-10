542749b67468c34c0ed0e8a37cae811fd2e8d50d
kub938 <kub938@naver.com>
2023-05-11 01:58:59 +0900

    식당 검색시 json 형식으로 출력

--- 캡스톤 디자인/test/test.js
+++ 캡스톤 디자인/test/test.js
@@ -1,4 +1,32 @@
-const local_Id = document.querySelector('#local_name').value;
+const secret_Id = 'nBcrF_omljd_AL2WOV0n'
+const secret_Pw = 'QuroIWuHzI'
+const local_Id = document.querySelector('#local_name');
 const submit_Local = document.querySelector('#local_submit');
 
-console.log(local_Id);
+
+
+function submit() {
+    local = local_Id.value;
+    console.log(local);
+
+
+}
+
+submit_Local.addEventListener('click', submit);
+
+const getLocalData = async () => {
+    const config = {
+        headers: {
+            Accept: 'applicataion/json',
+            'X-Naver-Client-Id': secret_Id,
+            'X-Naver-Client-Secret': secret_Pw,
+            display: 5,
+            start: 1,
+            sort: 'comment'
+        }
+    }
+    const res = await axios.get(`https://openapi.naver.com/v1/search/local.json?query=대전&display=5&start=1&sort=comment`)
+    console.log(res.data.title)
+}
+
+
